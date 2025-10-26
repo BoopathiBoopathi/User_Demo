fb68f6657dec4e16b349e633f998b3d54df0bfd8
Boopathi S <boopathiboo647@gmail.com>
2025-10-26 12:13:03 +0530

    package.json added for build

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(
 
 app.use(express.json());
 app.use(logger);
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use("/api/v1/auth", authRoutesV1);
 app.use("/api/v1/users", userRoutesV1);
@@ -28,6 +29,10 @@ app.use("/api/v1/users", userRoutesV1);
 
 const PORT = process.env.PORT || 5000;
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
+
 connectDB().then(() => {
-    app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
